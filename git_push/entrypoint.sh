a4b04a9dae743e770649aaa26abb5be53ec66afe
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-01-06 00:41:09 -0500

    remove dump

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -2,6 +2,6 @@
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add * && \
-git commit -m 'GitHub Action Push $*' > /dev/null 2>&1 && \
-git push > /dev/null 2>&1 && \
+git commit -m 'GitHub Action Push $*' && \
+git push && \
 echo 'SUCCESS!'
