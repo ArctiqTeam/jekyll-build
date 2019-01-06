c4b5fff792484d56485d0aee2d6cd4cb3d4382f3
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-01-06 00:53:50 -0500

    change branch

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+BRANCH=$(git rev-parse --abbrev-ref HEAD) && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add * && \
-git commit -m 'GitHub Action Push $*' && \
-git push && \
+git commit -m "GitHub Action Push $*" && \
+git push origin ${BRANCH} && \
 echo 'SUCCESS!'
