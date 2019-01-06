7e0e8adf13c147f657f209e570931b749bd03bf9
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-01-06 13:40:22 -0500

    remove and statements

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-BRANCH=$(git rev-parse --abbrev-ref HEAD) && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add * && \
-git commit -m "GitHub Action Push $*" && \
-git push origin ${BRANCH} && \
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add *
+git commit -m "GitHub Action Push $*"
+git push origin ${BRANCH}
 echo 'SUCCESS!'
