06940918b7460ce6e6666ab7192a6c5bbd4d27bb
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-18 18:32:02 -0400

    remove checkout

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -4,7 +4,6 @@ env
 BRANCH=$(sed 's/refs\/heads\///g' <<<"$GITHUB_REF")
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-git checkout ${BRANCH}
 git add *
 git commit -m "GitHub Action Push $*"
 git push origin ${BRANCH}
