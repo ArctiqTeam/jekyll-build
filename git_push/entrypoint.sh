410706971bc8657969978058d7b35c6af686ca53
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-18 16:05:17 -0400

    change push

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -e
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add *
 git commit -m "GitHub Action Push $*"
-git push origin ${BRANCH}
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/ArctiqTeam/ArctiqTeam.github.io.git" ${BRANCH}
+# git push origin ${BRANCH}
+echo 'git remote -v'
 echo 'SUCCESS!'
