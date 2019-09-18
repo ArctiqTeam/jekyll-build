e5a7e0343db6cf61be1756d990de40c5ccf155cd
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-18 16:28:16 -0400

    change push again

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -5,8 +5,8 @@ git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add *
 git commit -m "GitHub Action Push $*"
-echo 'git remote -v'
+git remote -v
 env
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/ArctiqTeam/ArctiqTeam.github.io.git" ${BRANCH}
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/ArctiqTeam/ArctiqTeam.github.io.git" ${GITHUB_REF}
 # git push origin ${BRANCH}
 echo 'SUCCESS!'
