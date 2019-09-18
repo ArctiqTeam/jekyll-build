f1b1c1bb9656fd36b7958466c0416564a873c527
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-18 17:20:25 -0400

    change BRANCH var

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -e
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-ls -l
-git status
-echo $BRANCH
+BRANCH=$(sed 's/refs\/heads\///g' <<<"$GITHUB_REF")
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add *
 git commit -m "GitHub Action Push $*"
-git remote rm origin
-git remote add origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git remote -v
-env
-git push
+git push origin ${BRANCH}
 echo 'SUCCESS!'
