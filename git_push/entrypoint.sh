a31c453903b2124ab1bf8ae3dad7c6c99900bd35
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-20 10:48:39 -0400

    verbos

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add *
+env
 git commit -m "GitHub Action Push $*"
 git push origin ${BRANCH}
 echo 'SUCCESS!'
