b22022d2bfab0c88da1ab3f9368373f2593d3d7e
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-20 10:56:26 -0400

    add branch

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add *
