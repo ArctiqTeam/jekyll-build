5ab951657080082263f45b6126c8267a133204aa
Tim Fairweather <tim.fairweather@arctiq.ca>
2019-09-20 11:09:34 -0400

    force push

--- git_push/entrypoint.sh
+++ git_push/entrypoint.sh
@@ -12,13 +12,8 @@ EOF
 chmod 600 $HOME/.netrc
 git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git config user.name "$GITHUB_ACTOR"
-if ! git diff --quiet
-then
-    git add *
-    env
-    git commit -m "GitHub Action Push $*"
-    git push --set-upstream origin ${BRANCH}
-else 
-  echo "Working tree clean. Nothing to commit."
-fi
+git add *
+env
+git commit -m "GitHub Action Push $*"
+git push --set-upstream origin ${BRANCH}
 echo 'SUCCESS!'
